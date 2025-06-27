200cf532f5f4d7d75bb8a79aa594cb9f392dec4f
freygold <ahmedtawfik833@gmail.com>
2025-06-27 23:00:19 +0300

    f

--- src/_components/Navigation.tsx
+++ src/_components/Navigation.tsx
@@ -10,9 +10,9 @@ function Navigation() {
             <FaBars />
          </div>
          <div className="flex mt-4 flex-col items-center gap-3 text-center">
-            <div className="h-[1px] bg-[#00000057] w-[100vw]"></div>
+            <div className="h-[1px] bg-[#00000057] dark:bg-white dark:opacity-30  w-[100vw]"></div>
             <h3 className="text-7xl font-[700]">THE BLOG</h3>
-            <div className="h-[1px] bg-[#00000057] w-[100vw]"></div>
+            <div className="h-[1px] bg-[#00000057] dark:bg-white dark:opacity-30 w-[100vw]"></div>
          </div>
       </div>
    );

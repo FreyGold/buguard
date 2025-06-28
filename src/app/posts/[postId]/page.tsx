e715eaee713f0fefe2a2cd8e401924c4d92b8b62
freygold <ahmedtawfik833@gmail.com>
2025-06-28 23:03:15 +0300

    final touch

--- src/app/posts/[postId]/page.tsx
+++ src/app/posts/[postId]/page.tsx
@@ -2,16 +2,20 @@ import { fetchPost } from "@/lib/api";
 import Image from "next/image";
 
 async function Page({ params }: { params: { postId: string } }) {
-   const { postId } = await params;
+   const { postId } = params;
    const { title, body } = await fetchPost(postId);
    const randomImage = Math.ceil(Math.random() * 10);
    return (
-      <div className="flex w-screen h-screen px-8 py-5 gap-8 flex-col">
-         {" "}
+      <div className="flex w-full h-screen px-8 py-5 gap-8 flex-col">
          <h5 className=" text-lg text-[#6941C6]">Sunday , 1 Jan 2023</h5>
          <h2 className="font-bold text-4xl leading-9">{title.toUpperCase()}</h2>
          <div className="relative aspect-video">
-            <Image src={`/${randomImage}.jpg`} alt="placeholder" fill />
+            <Image
+               src={`/${randomImage}.jpg`}
+               alt="placeholder"
+               fill
+               className="object-cover"
+            />
          </div>
          <p className="">{body}</p>
       </div>
